0f2cecfd99a394e6c52fc24e5e9988d643b86996
eloss <elhoussaineaitX@gmail.comm>
2022-02-24 18:45:10 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const choiceClick01 = document.querySelector(".choice-01");
 const choiceClick02 = document.querySelector(".choice-02");
 const choiceClick03 = document.querySelector(".choice-03");
 const listIcon = document.querySelector(".list-icon");
+const choiceBox = document.getElementById("choice-box");
 
 formBtn.addEventListener("click", function () {
   selectionMenu.classList.toggle("hidden");
@@ -13,10 +14,13 @@ formBtn.addEventListener("click", function () {
 
 choiceClick01.addEventListener("click", function () {
   selectionMenu.classList.add("hidden");
+  choiceBox.innerHTML = choiceClick01.innerHTML;
 });
 choiceClick02.addEventListener("click", function () {
   selectionMenu.classList.add("hidden");
+  choiceBox.innerHTML = choiceClick02.innerHTML;
 });
 choiceClick03.addEventListener("click", function () {
   selectionMenu.classList.add("hidden");
+  choiceBox.innerHTML = choiceClick03.innerHTML;
 });
